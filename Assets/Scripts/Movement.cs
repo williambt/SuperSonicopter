f66233e2530e0a94c94074998799d1aed5912c75
William <willnuno@gmail.com>
2018-09-24 20:56:15 -0300

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float yStart;
+    public float offset = 1f;
+    wrmhl device = new wrmhl();
+
+    public string portName = "COM3";
+    public int baudRate = 9600;
+    public int readTimeout = 20;
+    public int queueLength = 1;
+
+    float lastPos = 0;
+    public float sensitivity = 3;
+
+    float lerpStartY = 0;
+    float lerpTargetY = 0;
+    float lerpT = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        yStart = transform.position.y;
+        device.set(portName, baudRate, readTimeout, queueLength);
+        device.connect();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(lerpStartY != lerpTargetY)
+        {
+            lerpT += 0.08f;
+            transform.position = new Vector2(transform.position.x, Mathf.Lerp(lerpStartY, lerpTargetY, lerpT));
+        }
+
+        string rq = device.readQueue();
+        if (rq != "!" && rq != null)
+        {
+            float cm = long.Parse(rq) / 10.0f;
+            if (Mathf.Abs(lastPos - cm) >= sensitivity)
+            {
+                print(cm);
+                //transform.position = new Vector2(transform.position.x, yStart + (cm / 10.0f - 3.0f) * offset);
+                lerpTargetY = yStart + (cm / 10.0f - 3.0f) * offset;
+                lerpStartY = transform.position.y;
+                lerpT = 0;
+                lastPos = cm;
+            }
+        }
+	}
+
+    void OnApplicationQuit()
+    {
+        device.close();
+    }
+}
---
+++ Assets/Scripts/wrmhlComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wrmhlComponent
+{
+    wrmhl Device = new wrmhl(); // wrmhl is the bridge beetwen your computer and hardware.
+
+    [Tooltip("SerialPort of your device.")]
+    public string portName = "COM4";
+
+    [Tooltip("Baudrate")]
+    public int baudRate = 9600;
+
+
+    [Tooltip("Timeout")]
+    public int readTimeout = 20;
+
+    [Tooltip("QueueLenght")]
+    public int queueLenght = 1;
+
+    //use this to use the default COM4 and 9600
+    public wrmhlComponent()
+    {
+        Device.set(portName, baudRate, readTimeout, queueLenght); // This method set the communication with the following vars;
+                                                                  //                              Serial Port, Baud Rates, Read Timeout and QueueLenght.
+        Device.connect(); // This method open the Serial communication with the vars previously given.
+    }
+    public wrmhlComponent(string PortName, int BaudRate, int ReadTimeout, int QueueLenght)
+    {
+        Device.set(PortName, BaudRate, ReadTimeout, QueueLenght); // This method set the communication with the following vars;
+                                                                  //                              Serial Port, Baud Rates, Read Timeout and QueueLenght.
+        Device.connect(); // This method open the Serial communication with the vars previously given.
+    }
+    ~wrmhlComponent()
+    {
+        Device.close();
+    }
+    public string Read()
+    {
+        return Device.readQueue();
+    }
+    public void Write(string dataToSend)
+    {
+        Device.send(dataToSend);
+    }
+}
