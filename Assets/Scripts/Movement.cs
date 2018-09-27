8c80e8e4e20a6bbc82f6c650da3d07823b52f675
Gabriel Souza da Silva <GABRIELSOUZAS@edu.unisinos.br>
2018-09-27 11:04:19 -0300

    Inicio

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+	public GameObject bullet;
+	AudioSource source;
+	public AudioClip explosion;
+	public Sprite dead;
+
+
     float yStart;
     public float offset = 1f;
     wrmhl device = new wrmhl();
@@ -26,6 +32,7 @@ public class Movement : MonoBehaviour
         yStart = transform.position.y;
         device.set(portName, baudRate, readTimeout, queueLength);
         device.connect();
+		source = gameObject.AddComponent<AudioSource> ();
     }
 
     // Update is called once per frame
@@ -33,23 +40,33 @@ public class Movement : MonoBehaviour
     {
         if(lerpStartY != lerpTargetY)
         {
-            lerpT += 0.08f;
+            lerpT += 0.15f;
             transform.position = new Vector2(transform.position.x, Mathf.Lerp(lerpStartY, lerpTargetY, lerpT));
         }
 
-        string rq = device.readQueue();
-        if (rq != "!" && rq != null)
+		string fire = device.readQueue();
+		//print (fire);
+		if (fire == null) {
+			return;
+		}
+		char[] delim = { '|' };
+		string[] a = fire.Split (delim);
+		if (a[0] == "fire") 
+		{
+			Fire ();
+		}
+		if (a[1] != "!" && a[1] != null)
         {
-            float cm = long.Parse(rq) / 10.0f;
-            if (Mathf.Abs(lastPos - cm) >= sensitivity)
-            {
-                print(cm);
-                //transform.position = new Vector2(transform.position.x, yStart + (cm / 10.0f - 3.0f) * offset);
-                lerpTargetY = yStart + (cm / 10.0f - 3.0f) * offset;
-                lerpStartY = transform.position.y;
-                lerpT = 0;
-                lastPos = cm;
-            }
+			float cm = float.Parse(a[1]) / 10.0f;
+			if (Mathf.Abs(lastPos - cm) >= sensitivity)
+			{
+				//print(cm);
+				//transform.position = new Vector2(transform.position.x, yStart + (cm / 10.0f - 3.0f) * offset);
+				lerpTargetY = yStart + (cm / 10.0f - 3.0f) * offset;
+				lerpStartY = transform.position.y;
+				lerpT = 0;
+				lastPos = cm;
+			}
         }
 	}
 
@@ -57,4 +74,19 @@ public class Movement : MonoBehaviour
     {
         device.close();
     }
+	void Fire ()
+	{
+		GameObject.Instantiate (bullet, transform.position,transform.rotation);
+	}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		print (col.gameObject.name);
+		source.clip = explosion;
+		source.loop = false;
+		GetComponent<SpriteRenderer> ().sprite = dead;
+
+		GetComponent<Collider2D> ().enabled = false;
+		source.Play ();
+	}
+
 }
---
+++ Assets/Scripts/Scenario.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenario : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Scroll()
+	{
+		transform.Translate (new Vector2 (-speed, 0)* Time.deltaTime );
+	}
+}
